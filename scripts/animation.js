43ccd0872a7c3bf7fb8f24e1c681f14d78711cd1
Yordan Stoyanov <y.s.stoyanov@hotmail.com>
2019-03-31 21:23:08 +0200

    changes

--- scripts/animation.js
+++ scripts/animation.js
@@ -3,7 +3,7 @@ TweenMax.from("#location", 1, {ease:Expo.easeOut, top:800, opacity:0, scale:0, d
 TweenMax.from("#temperature", 1, {ease:Expo.easeOut, top:800, opacity:0, scale:0, delay:0.9});
 TweenMax.from("#wind", 1, {ease:Expo.easeOut, top:800, opacity:0, scale:0, delay:1.2});
 TweenMax.from("#clouds", 1, {ease:Expo.easeOut, top:800, opacity:0, scale:0, delay:1.5});
-TweenMax.from("#suitable", 1, {ease:Expo.easeOut, top:800, opacity:0, scale:0, delay:1.8});
+TweenMax.from("#advice", 1, {ease:Expo.easeOut, top:800, opacity:0, scale:0, delay:1.8});
 
 
 
